6de8ed4987053f92ad5a59bc5648cdfc183228f4
Carlos Madrigal <carlosmad03@gmail.com>
2021-06-30 00:25:26 -0500

    menu de navegacion, vista create alumnos y select de cursos para vista create alumnos

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using curso_asp_netcore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace curso_asp_netcore.Controllers
 {
@@ -46,6 +47,44 @@ namespace curso_asp_netcore.Controllers
 
                 //Aqui estamos especificando entre comillas la vista que queremos mostrar
                 return View("MultiAlumno", _Context.Alumnos);
+        }
+
+        public IActionResult Create()
+        {
+            List<Curso> listaCursos = null;
+            listaCursos = (from c in _Context.Cursos
+                            select new Curso
+                            {
+                                Id = c.Id,
+                                Nombre = c.Nombre
+                            }).ToList();
+
+            List<SelectListItem> items = listaCursos.ConvertAll(d => {
+                return new SelectListItem(){
+                    Text = d.Nombre.ToString(),
+                    Value = d.Id.ToString(),
+                    Selected = false
+                };
+            });
+
+            ViewBag.items = items;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Alumno alumno)
+        {
+            if(ModelState.IsValid)
+            {
+                _Context.Alumnos.Add(alumno);
+                _Context.SaveChanges();
+                ViewBag.Mensaje = "Alumno Creado con Éxito";
+                return View("Index", alumno);
+            }
+            else
+            {
+                return View(alumno);
+            }
         }
     }
 }
--- Models/Alumno.cs
+++ Models/Alumno.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace curso_asp_netcore.Models
 {
   //:ObjetoEscuelaBase indica que la clase Alumno hereda de ObjetoEscuelaBAse
     public class Alumno:ObjetoEscuelaBase 
     {
+        [Required]
+        public override string Nombre { get; set; }
+
         public string CursoId { get; set; }
 
         public Curso Curso { get; set; }
