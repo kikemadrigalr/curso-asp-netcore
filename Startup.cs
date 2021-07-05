bbe22357dd9ef40b8270c6acd170cfbce2c73b2c
Carlos Madrigal <carlosmad03@gmail.com>
2021-07-05 18:59:53 -0500

    Conectando el proyecto a una base de datos real - MySql con entity framework core

--- Startup.cs
+++ Startup.cs
@@ -27,10 +27,24 @@ namespace curso_asp_netcore
         {
             services.AddControllersWithViews();
 
+        // ***********************IN MEMORY DATA BASE***********************************
             //configuracion del servicio para el contexto de base de datos
+            // services.AddDbContext<EscuelaContext>(
+            //     options => options.UseInMemoryDatabase(databaseName: "TestDB")
+            // );
+
+
+            // **********************MY SQL DATA BASE**********************************
+            string mySqlConnectionStr = ConfigurationExtensions.GetConnectionString(this.Configuration, "DefaultConnectionstring");
             services.AddDbContext<EscuelaContext>(
-                options => options.UseInMemoryDatabase(databaseName: "TestDB")
+                options => options.UseMySql(mySqlConnectionStr, ServerVersion.AutoDetect(mySqlConnectionStr))
             );
+
+            // *************************SQL SERVER DATA BASE***************************************
+            // string connString = ConfigurationExtensions.GetConnectionString(this.Configuration, "DefaultConnectionstring");
+            // services.AddDbContext<EscuelaContext>(
+            //     options => options.UseSqlServer(connString))
+            // );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
